2fd435da51bba062523b02453ee7097a4645b89d
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2026-02-11 15:16:26 -0500

    Inicio extraccion de datos pacsis

--- QRPython/generate_qr.py
+++ QRPython/generate_qr.py
@@ -30,7 +30,7 @@ try:
         direccion = ubicacion['direccion']
         
         # Comprobar si la dirección es "C Sicologia"
-        if direccion == "Coliseo Sala de Robótica":
+        if direccion == "B Oficina líder Sección":
             unidades_filtradas.append(unit)
 
 
@@ -59,7 +59,7 @@ try:
         estado_producto = unit['estado']
         
         # Filtrar solo las unidades con la dirección "C psicología"
-        if direccion == "Coliseo Sala de Robótica":
+        if direccion == "B Oficina líder Sección":
             count_unidades += 1  # Incrementar el contador
             
             # Generar URL específica para cada unidad usando el id_unidad
